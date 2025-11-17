9d2bae14a31c3501f59c86ee896d1663bcdea089
aniavg <aniavg2003@gmail.com>
2025-11-17 11:46:03 +0300

    smth wrong in demodulation

--- fm_receiver/receiver_for_fm.py
+++ fm_receiver/receiver_for_fm.py
@@ -6,33 +6,34 @@ from scipy.signal import hilbert
 msg_signal, sample_rate = sf.read("original_audio.mp3")
 
 if msg_signal.ndim > 1:
-    msg_signal = msg_signal[0]
-
-####
-n = np.max(np.abs(msg_signal))
-if n > 0:
-    msg_signal = msg_signal / n
-else:
-    raise ValueError("Audio signal is zero")
-#####
+    msg_signal = msg_signal[:, 0]
 
 msg_signal = msg_signal / np.max(np.abs(msg_signal))
-T = 1 / sample_rate
-freq = np.fft.fftfreq(len(msg_signal), T)
-Am = np.max(np.abs(msg_signal))
 
-carrier_freq = 5 * np.max(np.abs(freq))
+T = 1 / sample_rate
+# freq = np.fft.fftfreq(len(msg_signal), T)
+
+# carrier_freq = 10 * np.max(np.abs(freq))
+carrier_freq = 20000
 Ac = 1
 time = np.arange(len(msg_signal)) / sample_rate
 
-kf = 100
-integral_m_t = np.cumsum(msg_signal) * (1 / sample_rate)
+kf = 2000
+integral_msg = np.cumsum(msg_signal) * (1 / sample_rate)
 
-fm_signal = Ac * np.cos(2 * np.pi * carrier_freq * time + 2 * np.pi * kf * integral_m_t)
+fm_signal = Ac * np.cos(2 * np.pi * carrier_freq * time + 2 * np.pi * kf * integral_msg)
+print("msg signal shape:", msg_signal.shape)
+
+print("msg len:", len(msg_signal))
+print("fm len:", len(fm_signal))
+print("time len:", len(time))
 
 analytic = hilbert(fm_signal)
 phase = np.unwrap(np.angle(analytic))
-inst_freq = np.diff(phase) * sample_rate / (2 * np.pi)
+# inst_freq = np.diff(phase) * sample_rate / (2 * np.pi)
+inst_freq = np.diff(phase) / (2*np.pi*T)
+inst_freq = np.concatenate(([inst_freq[0]], inst_freq))
+
 recovered = inst_freq - carrier_freq
 recovered = recovered / kf
 
@@ -51,7 +52,9 @@ noisy_fm_signal = adding_awgn(fm_signal, 30)
 
 analytic = hilbert(noisy_fm_signal)
 phase = np.unwrap(np.angle(analytic))
-inst_freq = np.diff(phase) * sample_rate / (2 * np.pi)
+# inst_freq = np.diff(phase) * sample_rate / (2 * np.pi)
+inst_freq = np.diff(phase) / (2*np.pi*T)
+inst_freq = np.concatenate(([inst_freq[0]], inst_freq))
 recovered_noisy = inst_freq - carrier_freq
 recovered_noisy = recovered_noisy / kf
 
@@ -70,7 +73,6 @@ def plotting(t, signal, title):
     plt.ylabel("Amplitude")
     plt.grid(True)
 
-
 plt.figure(figsize = (12, 8))
 plt.subplot(3, 1, 1)
 plotting(time, msg_signal, "Message Signal")
@@ -85,22 +87,22 @@ plotting(time[start:end], msg_signal[start:end], "Message Signal (Zoomed)")
 plt.subplot(2, 1, 2)
 plotting(time[start:end], recovered[start:end], "Recovered Signal (Zoomed)")
 
-plt.figure(figsize = (10, 8))
-plt.subplot(2, 1, 1)
-plotting(time, noisy_fm_signal, "Noisy Signal")
-plt.subplot(2, 1, 2)
-plotting(time[start:end], noisy_fm_signal[start:end], "Noisy Signal (Zoomed)")
-
- # Recovered Signal
-plt.figure(figsize = (18, 8))
-plt.subplot(2, 2, 1)
-plotting(time, recovered, "Recovered Message Signal")
-plt.subplot(2, 2, 2)
-plotting(time[start:end], recovered[start:end], "Recovered Message Signal (Zoomed)")
-plt.subplot(2, 2, 3)
-plotting(time, recovered_noisy, "Recovered Noisy Signal")
-plt.subplot(2, 2, 4)
-plotting(time[start:end], recovered_noisy[start:end], "Recovered Noisy Signal (Zoomed)")
+# plt.figure(figsize = (10, 8))
+# plt.subplot(2, 1, 1)
+# plotting(time, noisy_fm_signal, "Noisy Signal")
+# plt.subplot(2, 1, 2)
+# plotting(time[start:end], noisy_fm_signal[start:end], "Noisy Signal (Zoomed)")
+#
+# # Recovered Signal
+# plt.figure(figsize = (18, 8))
+# plt.subplot(2, 2, 1)
+# plotting(time, recovered, "Recovered Message Signal")
+# plt.subplot(2, 2, 2)
+# plotting(time[start:end], recovered[start:end], "Recovered Message Signal (Zoomed)")
+# plt.subplot(2, 2, 3)
+# plotting(time, recovered_noisy, "Recovered Noisy Signal")
+# plt.subplot(2, 2, 4)
+# plotting(time[start:end], recovered_noisy[start:end], "Recovered Noisy Signal (Zoomed)")
 
 plt.tight_layout()
 plt.show()
