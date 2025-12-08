eca586a100a7b44a71a46ffcd8d0e6c18cfd16e8
aniavg <aniavg2003@gmail.com>
2025-12-08 14:56:19 +0300

    added same for 4QAM

--- QAM/ber_snr_qam.py
+++ QAM/ber_snr_qam.py
@@ -1,16 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import string
-import random
 
-def generate_bits(min_len=32, max_len=1024):
+def generate_bits(n, min_len=32, max_len=1024):
     L = np.random.randint(min_len, max_len + 1)
-    L += (-L) % 4
+    L += (-L) % n
 
     random_bits = np.random.randint(0, 2, size=L, dtype=np.uint8)
     return random_bits
 
+# 4 QAM
+def qam4_mod(bits):
+    bits = bits.reshape(-1, 2)
+    mapping = {
+        (0, 0): -1-1j,
+        (0, 1): -1+1j,
+        (1, 0): 1-1j,
+        (1, 1): 1+1j
+    }
+    return np.array([mapping[tuple(b)] for b in bits])
 
+def qam4_demod(symbols):
+    all_bits = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+
+    const_points = np.array([
+        -1-1j, -1+1j, 1-1j, 1+1j
+    ])
+
+    decoded_bits = []
+    for s in symbols:
+        idx = np.argmin(np.abs(const_points - s))
+        decoded_bits.append(all_bits[idx])
+    return np.vstack(decoded_bits).flatten()
+
+# 16 QAM
 def qam16_mod(bits):
     bits = bits.reshape(-1, 4)
     mapping = {
@@ -44,7 +66,6 @@ def qam16_demod(symbols):
     for s in symbols:
         idx = np.argmin(np.abs(const_points - s))
         decoded_bits.append(all_bits[idx])
-
     return np.vstack(decoded_bits).flatten()
 
 
@@ -57,16 +78,36 @@ def add_awgn(signal, snr_db):
     )
     return signal + noise
 
-def simulate():
-
-    snr_list = [0, 5, 10, 15, 20, 25]
+def simulate_4qam():
+    snr_list = [0, 5, 10, 15, 20, 25, 30, 35]
     ber_list = []
-
     for snr in snr_list:
         errors = 0
         total = 0
         for _ in range(1000):
-            bits = generate_bits()
+            bits = generate_bits(2)
+            tx = qam4_mod(bits)
+            rx = add_awgn(tx, snr)
+            decoded_bits = qam4_demod(rx)
+
+            errors += np.sum(bits != decoded_bits)
+            total += len(bits)
+
+        ber = errors / total
+        ber_list.append(ber)
+        print(f"SNR_4={snr} dB → BER_4={ber:.6f}")
+    return snr_list, ber_list
+
+
+def simulate_16qam():
+
+    snr_list = [0, 5, 10, 15, 20, 25, 30, 35]
+    ber_list = []
+    for snr in snr_list:
+        errors = 0
+        total = 0
+        for _ in range(1000):
+            bits = generate_bits(4)
             tx = qam16_mod(bits)
             rx = add_awgn(tx, snr)
             decoded_bits = qam16_demod(rx)
@@ -76,16 +117,17 @@ def simulate():
 
         ber = errors / total
         ber_list.append(ber)
-        print(f"SNR={snr} dB → BER={ber:.6f}")
-
+        print(f"SNR_16={snr} dB → BER_16={ber:.6f}")
     return snr_list, ber_list
 
-snrs, bers = simulate()
+snrs_4qam, bers_4qam = simulate_4qam()
+snrs_16qam, bers_16qam = simulate_16qam()
 
 plt.figure()
-plt.semilogy(snrs, bers, marker='o')
+plt.semilogy(snrs_4qam, bers_4qam, marker='o')
+plt.semilogy(snrs_16qam, bers_16qam, marker='o')
 plt.xlabel("SNR (dB)")
 plt.ylabel("BER")
-plt.title("BER vs SNR (16-QAM)")
+plt.title("BER vs SNR (4-QAM and 16-QAM)")
 plt.grid(True)
 plt.show()
